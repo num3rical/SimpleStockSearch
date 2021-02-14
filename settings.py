d6f5a2010f446f04af39d9ebc5a7df79a1311844
num3rical <46986196+num3rical@users.noreply.github.com>
2021-02-14 13:09:57 -0500

    Add comments and partly adhere to PEP 8
    Added comments in the code to improve readability, and also changed styling of the code to adhere to PEP 8 (excluding line length, because it makes it harder to read in some places).

--- settings.py
+++ settings.py
@@ -2,20 +2,46 @@ import sys
 from os import path
 from json import (load as jsonload, dump as jsondump)
 
-SITE_KEYS = ['-TRADINGVIEW-', '-TWITTER-', '-YAHOOFINANCE-', '-MARKETWATCH-', '-SEEKINGALPHA-', '-STOCKCHARTS-', '-OPENINSIDER-', '-FINVIZ-', '-ROBINHOOD-']
 SETTINGS_FILE = path.join(path.dirname(sys.executable), r'config.cfg')
-DEFAULT_SETTINGS = {'theme': 'SSSDefault', 'enabled_sites_keys': ['-TRADINGVIEW-', '-TWITTER-', '-YAHOOFINANCE-']}
-SETTINGS_KEYS_TO_ELEMENT_KEYS = {'theme': '-THEME-'}
-print(SETTINGS_FILE)
+
+SITE_KEYS = [
+    '-TRADINGVIEW-',
+    '-TWITTER-',
+    '-YAHOOFINANCE-',
+    '-MARKETWATCH-',
+    '-SEEKINGALPHA-',
+    '-STOCKCHARTS-',
+    '-OPENINSIDER-',
+    '-FINVIZ-',
+    '-ROBINHOOD-'
+]
+
+DEFAULT_SETTINGS = {
+    'theme': 'SSSDefault',
+    'enabled_sites_keys': ['-TRADINGVIEW-', '-TWITTER-', '-YAHOOFINANCE-']
+}
+
+SETTINGS_KEYS_TO_ELEMENT_KEYS = {
+    'theme': '-THEME-'
+}
+
+
+# load_settings: Reads the settings file, sets the contents equal to the
+# 'settings' variable, and returns it.
 def load_settings(settings_file, default_settings):
     try:
         with open(settings_file, 'r') as f:
             settings = jsonload(f)
+    # If the settings file cannot be found, creates one with default settings.
     except Exception as e:
         settings = default_settings
         save_settings(settings_file, settings, None)
     return settings
 
+
+# save_settings: Gets the value of each setting from the 'settings' variable,
+# adds each enabled website to 'enabled_sites_keys', and saves to the settings
+# file.
 def save_settings(settings_file, settings, values):
     if values:
         for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:
@@ -26,7 +52,7 @@ def save_settings(settings_file, settings, values):
 
         for key in values:
             if key in SITE_KEYS:
-                if values[key] == True:
+                if values[key]:
                     if key not in settings['enabled_sites_keys']:
                         try:
                             settings['enabled_sites_keys'].append(key)
@@ -41,6 +67,6 @@ def save_settings(settings_file, settings, values):
                         except Exception as e:
                             print(e)
                             print(f'Problem updating settings from window values. Key = {key}')
-    
+
     with open(settings_file, 'w') as f:
         jsondump(settings, f)
